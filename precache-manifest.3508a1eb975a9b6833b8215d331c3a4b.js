9fec625c95dcdadfe1c8a935234f502168845f1f
Ernesto <ecalderoncc@gmail.com>
2020-08-04 19:59:31 +1000

    Updates

---
+++ precache-manifest.3508a1eb975a9b6833b8215d331c3a4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b61ab6554bfd53cd3a3fda389a2c652a",
+    "url": "/mini-project_monster-rolodex/index.html"
+  },
+  {
+    "revision": "dba07c9c7829cc42ac9a",
+    "url": "/mini-project_monster-rolodex/static/css/main.6c16463e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/mini-project_monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mini-project_monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dba07c9c7829cc42ac9a",
+    "url": "/mini-project_monster-rolodex/static/js/main.2bb457a8.chunk.js"
+  },
+  {
+    "revision": "14ccc9e824eb91531b67",
+    "url": "/mini-project_monster-rolodex/static/js/runtime-main.a7886af2.js"
+  }
+]);
